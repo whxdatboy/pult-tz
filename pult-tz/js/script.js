08117438bcd807c9b69ec7b12faef2c1b0253051
whxdatboy <smurf1osnya@gmail.com>
2021-04-02 12:19:55 +0300

    add footer

--- pult-tz/js/script.js
+++ pult-tz/js/script.js
@@ -1,5 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+  const accessoriesLink = document.querySelectorAll('.accessories-link'),
+        accessoriesImg = document.querySelector('.accessories__img');
+
+  const data = [
+    {
+      src: 'img/product/micro.jfif',
+    },
+    {
+      src: 'img/product/micro-se.jfif',
+    },
+    {
+      src: 'img/product/adiva.jfif',
+    },
+    {
+      src: 'img/product/adiva-se.jfif',
+    },
+    {
+      src: 'img/product/micro-droplet.jfif',
+    },
+    {
+      src: 'img/product/micro-se-droplet.jfif',
+    },
+    {
+      src: 'img/product/adiva-droplet.jfif',
+    },
+    {
+      src: 'img/product/adiva-se-droplet.jfif',
+    },
+    {
+      src: 'img/product/strada-2.jfif',
+    },
+    {
+      src: 'img/product/subwoofers.jfif',
+    },
+    {
+      src: 'img/product/accessories.jfif',
+    },
+  ]
+
   const swiper = new Swiper('.slider', {
     //loop: true,
     preloadImages: false,
@@ -9,10 +48,13 @@ document.addEventListener("DOMContentLoaded", function () {
       el: '.slider-pagination',
       clickable: true,
     },
-
-    // autoplay: {
-    //   delay: 5000,
-    // },
   });
+
+  accessoriesLink.forEach((link, i) => {
+    link.addEventListener('mouseover', function() {
+      accessoriesImg.src = data[i].src;
+    });
+  });
+
 
 });
